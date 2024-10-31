43b3bda6f86e87bb361df862acaff534a65ef27f
Kindsu <KindsuCSM@users.noreply.github.com>
2024-10-31 13:28:17 +0100

    Update Menu.cs

--- vista/Menu.cs
+++ vista/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _02_CristinaSanchez_GestorCatalogo.modelo;
 
 namespace _02_CristinaSanchez_GestorCatalogo.vista
 {
@@ -52,8 +53,58 @@ namespace _02_CristinaSanchez_GestorCatalogo.vista
 
         private void darAlta()
         {
-            
+            Console.WriteLine("1 - Solista");
+            Console.WriteLine("2 - Banda");
+            int opcion = Int32.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    ArtistaSolista solista = introducirDatosSolista();
+                    break;
+                case 2:
+                    ArtistaBanda banda = introducirDatosBanda();
+                    break;
+                default:
+                    break;
+            }
+
+
         }
+
+        private ArtistaSolista introducirDatosSolista()
+        {
+            int contador = 0;
+            bool estaActivo;
+            
+            Console.WriteLine("Nombre real:\t");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Instrumento:\t");
+            String instrumento = Console.ReadLine();
+            Console.WriteLine("Nombre artistico:\t");
+            String nombreArtistico = Console.ReadLine();
+            Console.WriteLine("Año de incicios:\t");
+            int anioInicio = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Discografía:\t");
+            String disgrafico = Console.ReadLine();
+            Console.WriteLine("Numero de discos:\t");
+            int numDiscos = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("¿Se encuentra activo?(Si-No)");
+            string active = Console.ReadLine();
+            estaActivo = (active == "Si")? true : false;
+            Console.WriteLine("Genero musical:");
+            foreach (Artista.GeneroMusical genero in Enum.GetValues(typeof(Artista.GeneroMusical)))
+            {
+                Console.WriteLine($"{contador} - {genero}");
+                contador++;
+            }
+            int genero = Int32.Parse(Console.ReadLine());
+            Artista.GeneroMusical generoMusical = (Artista.GeneroMusical)genero;
+            
+            return new ArtistaSolista(nombre, instrumento, nombreArtistico, anioInicio, disgrafico, numDiscos, generoMusical ,estaActivo);
+        }
+        
+        
 
         private void buscarElemento()
         {
